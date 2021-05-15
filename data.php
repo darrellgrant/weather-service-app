7fe345d7ed24bf046679075f2337523c72e98f80
Darrell Grant <dlsan3000@gmail.com>
2021-05-15 19:06:36 -0400

    add gitignore

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+
+$appid = "d384f81f1bdc231bcfb8532ef31237f0";
+
+header("Content-Type: application/json");
+
+echo json_encode($appid);
--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-let button = document.querySelector("#button");
-let inputValue = document.querySelector("#input");
-let temp = document.querySelector(".temp");
-let cityname = document.querySelector(".cityname");
-let desc = document.querySelector(".desc");
+function getData(callback) {
+  fetch("data.php")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      callback(data);
+      console.log(data);
+    });
+}
 
-button.addEventListener("click", function () {
-  let request = new XMLHttpRequest();
-  request.open(
-    "GET",
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-      inputValue.value +
-      "&appid=d384f81f1bdc231bcfb8532ef31237f0&units=imperial"
-  );
-  request.onload = function () {
-    let response = request.response;
-    let parsedData = JSON.parse(response);
-    let nameValue = parsedData["name"];
-    let tempValue = parsedData["main"]["temp"];
-    let descValue = parsedData["weather"][0]["description"];
-    console.log(parsedData);
-    cityname.innerHTML = `${nameValue}`;
-    temp.innerHTML = `Temperature: ${tempValue}`;
-    desc.innerHTML = `Description: ${descValue}`;
-  };
-  request.send();
+getData(function (myData) {
+  console.log(myData);
+  let myAppiKey = myData;
+  console.log(myAppiKey);
+  let button = document.querySelector("#button");
+  let inputValue = document.querySelector("#input");
+  let temp = document.querySelector(".temp");
+  let cityname = document.querySelector(".cityname");
+  let desc = document.querySelector(".desc");
+
+  button.addEventListener("click", function () {
+    let request = new XMLHttpRequest();
+    request.open(
+      "GET",
+      `http://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=${myAppiKey}&units=imperial`
+    );
+    request.onload = function () {
+      let response = request.response;
+      let parsedData = JSON.parse(response);
+      console.log(parsedData);
+      let nameValue = parsedData["name"];
+      let tempValue = parsedData["main"]["temp"];
+      let descValue = parsedData["weather"][0]["description"];
+      console.log(parsedData);
+      cityname.innerHTML = `${nameValue}`;
+      temp.innerHTML = `Temperature: ${tempValue}`;
+      desc.innerHTML = `Description: ${descValue}`;
+    };
+    request.send();
+  });
 });
